b28ddbf5db4b3d93b0fc346a0d64601a244328be
shakibkumnale <shakibkumnali@gmail.com>
2025-07-07 14:06:03 +0530

    change the cofiguration vercel  2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [tailwindcss(),
+   plugins: [tailwindcss(),
     react()],
-    server: {
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
